d87de7d191d75ada53369dd798e37f23bddbd20d
Aryan Panchal <98644643+aryannpanchal@users.noreply.github.com>
2025-06-27 11:56:58 +0530

    .

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-inter)", "sans-serif"],
+        serif: ["var(--font-playfair)", "serif"],
+      },
+    },
+  },
+  plugins: [],
+};
